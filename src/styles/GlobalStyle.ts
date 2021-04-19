683c02bf6f509fec044eeb061e7b0ac2e21da25d
Vojtěch Vidra <vojtechvidra@gmail.com>
2021-04-19 06:47:44 +0200

    feat(nextjs): setup with TS and styled components
    Add NextJS setup with TypeScript and styled-components

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
